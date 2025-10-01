e56f54ad3b977efb4da1c67919ade1447bd0b96c
mozxammil <mdmozammil605@gmail.com>
2025-10-01 14:22:42 -0200

    bug fixed

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -23,9 +23,7 @@ export const login = (req: Request, res: Response) => {
         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         maxAge: 24 * 60 * 60 * 1000,
         path: '/',
-        ...(process.env.NODE_ENV === 'production' && process.env.COOKIE_DOMAIN
-          ? { domain: process.env.COOKIE_DOMAIN }
-          : {}),
+        domain: process.env.FRONT_END_URL
       })
       .status(200)
       .json({ success: true, message: 'Login successful' });
